21b81060e606eee330a19a5496830fa26d123e7a
Monerovirus <monerovirus@gmx.com>
2021-12-14 08:26:08 -0800

    updated logging setup to be more compatible

--- dca_pipeline.py
+++ dca_pipeline.py
@@ -1,12 +1,16 @@
-# dependent on Path (pip install pathlib)
-# and cbpro (pip install git+git://github.com/Monerovirus/coinbasepro-python.git)
+# dependent on Path (pip3 install pathlib)
+# and cbpro (pip3 install git+git://github.com/Monerovirus/coinbasepro-python.git)
 
 import math, os, sys, logging, cbpro, csv
 import json_io
 from coinbase_pro_tasks import cbpPlaceOrder, cbpGetFiatBalance, cbpTryDepositFromBank, cbpTryWithdrawCrypto
 
-logging.basicConfig(filename='log.txt', encoding='utf-8', level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
-logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+handler = logging.FileHandler('log.txt', 'w', 'utf-8')
+handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S'))
+logger.addHandler(handler)
+logger.addHandler(logging.StreamHandler(sys.stdout))
 
 def succeeded(data):
     if "Error" in data and data["Error"] != None:
@@ -25,12 +29,14 @@ def writeToCsv(fileName, data):
 def runTasks(amount):
     auth_info = json_io.getJsonFile("auth.json")
     coinbase_auth_info = auth_info["coinbase"]
-    #cb_client = cbpro.AuthenticatedClient(coinbase_auth_info["key"], coinbase_auth_info["secret"], coinbase_auth_info["password"])
-    cb_client = cbpro.AuthenticatedClient(coinbase_auth_info["key"], coinbase_auth_info["secret"], coinbase_auth_info["password"], api_url="https://api-public.sandbox.pro.coinbase.com")
+    cb_client = cbpro.AuthenticatedClient(coinbase_auth_info["key"], coinbase_auth_info["secret"], coinbase_auth_info["password"])
     settings = json_io.getJsonFile("settings.json")
 
 #start
 if len(sys.argv) != 2:
     print("Usage: python coinbasepro-dca.py [fiat amount]")
 else:
-    runTasks(int(sys.argv[1]))
+    try:
+        runTasks(int(sys.argv[1]))
+    except Exception as e:
+        logging.error(f"Error occurred: {str(e)}.")
