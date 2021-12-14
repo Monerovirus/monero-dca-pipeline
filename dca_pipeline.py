2dee9b6bd0bb6d931fdd8b73294c27cb4e95b295
Monerovirus <monerovirus@gmx.com>
2021-12-13 20:09:32 -0800

    added function for writing/appending a csv file

--- dca_pipeline.py
+++ dca_pipeline.py
@@ -1,7 +1,7 @@
 # dependent on Path (pip install pathlib)
 # and cbpro (pip install git+git://github.com/Monerovirus/coinbasepro-python.git)
 
-import math, os, sys, logging, cbpro
+import math, os, sys, logging, cbpro, csv
 import json_io
 from coinbase_pro_tasks import cbpPlaceOrder, cbpGetFiatBalance, cbpTryDepositFromBank, cbpTryWithdrawCrypto
 
@@ -10,17 +10,24 @@ logging.getLogger().addHandler(logging.StreamHandler(sys.stdout))
 
 def succeeded(data):
     if "Error" in data and data["Error"] != None:
-        logging.error(taskResult.data["Error"])
+        logging.error(data["Error"])
         return False
     return True
+
+def writeToCsv(fileName, data):
+    isNew = not os.path.exists(fileName)
+    with open(fileName, 'a+', newline='') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames=data.keys())
+        if isNew:
+            writer.writeheader()
+        writer.writerow(data)
 
 def runTasks(amount):
     auth_info = json_io.getJsonFile("auth.json")
     coinbase_auth_info = auth_info["coinbase"]
-    cb_client = cbpro.AuthenticatedClient(coinbase_auth_info["key"], coinbase_auth_info["secret"], coinbase_auth_info["password"])
+    #cb_client = cbpro.AuthenticatedClient(coinbase_auth_info["key"], coinbase_auth_info["secret"], coinbase_auth_info["password"])
+    cb_client = cbpro.AuthenticatedClient(coinbase_auth_info["key"], coinbase_auth_info["secret"], coinbase_auth_info["password"], api_url="https://api-public.sandbox.pro.coinbase.com")
     settings = json_io.getJsonFile("settings.json")
-    
-    
 
 #start
 if len(sys.argv) != 2:
